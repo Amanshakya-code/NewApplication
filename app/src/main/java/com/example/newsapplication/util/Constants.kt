1c97b97ebf79ee76401ca2292a05e330e36dfca8
Aman Kumar <amanshakya.007@gmail.com>
2021-09-16 12:04:29 +0530

    final commit

--- app/src/main/java/com/example/newsapplication/util/Constants.kt
+++ app/src/main/java/com/example/newsapplication/util/Constants.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Constants  {
     companion object{
-        val API_KEY = "d84268fbea7f4d4f9f19b5eac07057b7"
+        val API_KEY = "a9ad92f0f5b6438cb6c47134e5410f69"
         const val BASE_URL = "https://newsapi.org"
         const val QUERY_PAGE_SIZE = 10
         var  COUNTRY_CODE = "in"
